84ef9346cf3780c412c68ece088c3a115412d1c8
Reonbalu <balu.reon@gmail.com>
2021-03-28 21:06:31 +0900

    1 file modified

--- src/components/molcules/user/UserIconWithName.jsx
+++ src/components/molcules/user/UserIconWithName.jsx
@@ -5,7 +5,9 @@ export const UserIconWithName = props => {
   const { image, name } = props;
   return (
     <Scontainer>
-      <SImg height={160} width={160} src={image} alt={name} />
+      <SSpan>
+        <SImg height={160} width={160} src={image} alt={name} />
+      </SSpan>
       <SName>{name}</SName>
     </Scontainer>
   );
@@ -15,9 +17,30 @@ const Scontainer = styled.div`
   text-align: center;
 `;
 
+const SSpan = styled.span`
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
+  cursor: pointer;
+  &:hover {
+    overflow: visible;
+    z-index: 1000; /* 浮かせるレイヤーの数 */
+    top: -5px; /* 縮小した画像を基点にずらす */
+    left: -5px; /* 　　　　　　〃　　　　　　 */
+  }
+`;
+
 const SImg = styled.img`
   border-radius: 50%;
+  &:hover {
+    background: #fff;
+    transform: scale(1.5, 1.5);
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.5);
+    -webkit-box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.5);
+  }
 `;
+
 const SName = styled.p`
   font-size: 18px;
   font-weight: bold;
